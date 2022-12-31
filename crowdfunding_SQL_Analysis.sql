908b7e3f2060cefb324c1275adf011e473bad5cc
ThomasPWink <thomas.p.wink@gmail.com>
2022-12-31 15:07:53 -0700

    updated challenge

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -1,18 +1,37 @@
 -- Challenge Bonus queries.
 -- 1. (2.5 pts)
 -- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
-
+select cf_id,
+	backers_count
+from campaign
+where (outcome = 'live')
+order by backers_count desc;
 
 
 -- 2. (2.5 pts)
 -- Using the "backers" table confirm the results in the first query.
-
+select count(backer_id)
+	count,
+	cf_id 
+from backers
+group by cf_id
+order by count(backer_id) desc;
 
 
 -- 3. (5 pts)
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
 
+select co.first_name, 
+	co.last_name, 
+	co.email,
+	c.goal-c.pledged as "Remaining Goal Amount"
+into email_contacts_remaining_goal_amount
+from contacts as co
+inner join campaign as c 
+	on (co.contact_id = c.contact_id)
+where (c.outcome = 'live')
+order by "Remaining Goal Amount" desc;
 
 
 
@@ -24,7 +43,20 @@
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
 
-
+select b.email,
+	b.first_name, 
+	b.last_name,
+	c.cf_id, 
+	c.company_name, 
+	c.description,
+	c.end_date,
+	c.goal-c.pledged as "Left of Goal"
+into email_backers_remaining_goal_amount
+from backers as b
+inner join campaign as c
+	on (b.cf_id = c.cf_id)
+where (c.outcome = 'live')
+order by b.email desc;
 
 -- Check the table
 
